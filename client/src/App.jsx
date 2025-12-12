7b33929f27490f044ff1db51d70b434ae26d95e5
Pragya Rai <163853565+raipragya@users.noreply.github.com>
2025-12-12 18:09:55 +0530

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,22 +20,27 @@ export default function App() {
     <BrowserRouter>
       <ScrollToTop />
       <Header />
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/search' element={<Search />} />
+
         <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
         </Route>
+
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
           <Route path='/update-post/:postId' element={<UpdatePost />} />
         </Route>
+
         <Route path='/projects' element={<Projects />} />
         <Route path='/post/:postSlug' element={<PostPage />} />
       </Routes>
+
       <Footer />
     </BrowserRouter>
   );
